4239ee4599b8365939b619bbfd7ca406798b5255
Yukkinho <alvarohenriquee73@gmail.com>
2025-01-30 21:27:31 -0300

    Faltava código na função de Exibir Texto na tela

--- app.js
+++ app.js
@@ -4,25 +4,19 @@ let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 
-//vou me matar
 
-
-
-
-if ('speechSynthesis' in window) {
-    let utterance = new SpeechSynthesisUtterance(texto);
-    utterance.lang = 'pt-BR';
-    utterance.rate = 1.2;
-    window.speechSynthesis.speak(utterance);
-
-} else {
-
-console.log("Web Speech API não suportada neste navegador.");
-
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
-
-
-
 
 
 
@@ -34,12 +28,7 @@ function exibirMensagemInicial() {
 
 
 
-
 exibirMensagemInicial();
-
-
-
-
 
 
 
@@ -62,9 +51,6 @@ function verificarChute() {
         limparCampo();
     }
 }
-
-
-
 
 
 
